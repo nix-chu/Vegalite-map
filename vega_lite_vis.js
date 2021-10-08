d86ae24c81edd730b5053c12ba6001ec507fe5f5
nix-chu <nmchua8@gmail.com>
2021-10-09 02:27:33 +1100

    Create concatenated views

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,3 @@
-let theMap = "map.vg.json";
-
-vegaEmbed("#theMap", theMap).then(function(result) {
-
-}).catch(console.error);
-
 let newMap = "newmap.vg.json";
 
 vegaEmbed("#newMap", newMap).then(function(result) {
