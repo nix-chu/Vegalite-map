c83a582cc11ed49ecf3a4459eef43177c43bc593
nix-chu <nmchua8@gmail.com>
2021-10-08 21:49:11 +1100

    Create new map json file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,9 @@ let theMap = "map.vg.json";
 vegaEmbed("#theMap", theMap).then(function(result) {
 
 }).catch(console.error);
+
+let newMap = "newmap.vg.json";
+
+vegaEmbed("#newMap", newMap).then(function(result) {
+
+}).catch(console.error);
